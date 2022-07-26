5d57342fd349b1f5c62fd0f75da8629c6d7ace78
Максим Александрович Мазурин <mazurima@internet.startatom.ru>
2022-07-26 13:48:06 +0300

    Создания чата на фрейме wcf 2.0 - написал конфиг

--- wcf_chat/ChatHost/Program.cs
+++ wcf_chat/ChatHost/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
 
 namespace ChatHost
 {
@@ -10,6 +11,12 @@ namespace ChatHost
     {
         static void Main(string[] args)
         {
+            using (var host = new ServiceHost(typeof(wcf_chat.ServiceChat)))
+            {
+                host.Open();
+                Console.WriteLine("Хост открыт!");
+                Console.ReadLine();
+            }
         }
     }
 }
