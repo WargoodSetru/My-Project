999045acd1c9a5914505db4e0ecb8ea3b0978c65
Максим Александрович Мазурин <mazurima@internet.startatom.ru>
2022-07-26 15:47:10 +0300

    Создания чата на фрейме wcf 3.0 - пишу клиент часть на XML

---
+++ wcf_chat/ChatClient/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool isConnected = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void ConnectUser() {
+
+            if (!isConnected)
+            {
+                isConnected = true;
+            }
+        }
+
+        void DisConnectUser() {}
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
